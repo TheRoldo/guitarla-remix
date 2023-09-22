8bc146b9870e8b86f3eb3f8840b1bc3f6ebcd4be
TheRoldo <nroldangaviria@gmail.com>
2023-09-22 16:50:12 -0500

    Proyecto Finalizado

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {
     Meta,
     Links,
@@ -53,8 +53,14 @@ export function links() {
 
 export default function App() {
 
-    const [carrito, setCarrito] = useState([])
+    const carritoLS = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('carrito')) ?? [] : [];
+    const [carrito, setCarrito] = useState(carritoLS)
     
+    useEffect(() => {
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }, [carrito])
+    
+
     const agregarCarrito = guitarra => {
         if(carrito.some(guitarraState => guitarraState.id === guitarra.id)) {
             //Iterar sobre el arreglo, e identificar el elemento dublicado
@@ -80,11 +86,29 @@ export default function App() {
         }
     } 
 
+    const actualizarCantidad = guitarra => {
+        const carritoActualizado = carrito.map(guitarraState => {
+            if(guitarraState.id === guitarra.id) {
+                guitarraState.cantidad = guitarra.cantidad
+            }
+            return guitarraState
+        })
+        setCarrito(carritoActualizado)
+    }
+
+    const eliminarGuitarra = id => {
+        const carritoActualizado = carrito.filter( guitarraState => guitarraState.id !== id )
+        setCarrito(carritoActualizado)
+    }
+
     return (
         <Document>
             <Outlet
                 context={{
-                    agregarCarrito
+                    agregarCarrito,
+                    carrito,
+                    actualizarCantidad,
+                    eliminarGuitarra
                 }}
             />
         </Document>
